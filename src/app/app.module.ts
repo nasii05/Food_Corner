09fececae1e869f750a1ce04fa3f6d543d4c8895
Nasih <iamnasih@gmail.com>
2023-05-24 15:21:02 +0530

    search functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './components/pages/search/search.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    SearchComponent,
     ],
   imports: [
     BrowserModule,
--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -12,4 +12,9 @@ export class FoodService {
   getAll():Food[]{
     return sample_foods;
   }
+
+  // seearch Food
+  getAllFoodBySearchTerm(searchTerm:string){
+    return this.getAll().filter( food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  }
 }
